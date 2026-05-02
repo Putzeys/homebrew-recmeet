42c86643635746cdacfbbdbf192713cbf01abd5b
putzeys <gputzeys@gmail.com>
2026-05-02 01:22:39 -0300

    Bump cask to v0.6.0

--- Casks/recmeet.rb
+++ Casks/recmeet.rb
@@ -1,6 +1,6 @@
 cask "recmeet" do
-  version "0.5.0"
-  sha256 "b18032f8f2a499fcc689abf7d623827f055f94aca4c5f4d2aee719288ca9a062"
+  version "0.6.0"
+  sha256 "a180f33e6961a2eea9ef38e0419d65c52c7d99790e4ee4d1d483a49a0b4f6515"
 
   url "https://github.com/Putzeys/Recmeet/releases/download/v#{version}/recmeet-macos.dmg"
   name "recmeet"
